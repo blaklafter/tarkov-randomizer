efb486794f3bfdc9c80abe651c93aa471621d69f
blaklafter <blaklafter@gmail.com>
2021-02-16 20:58:42 -0500

    webpage working

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,34 @@
-alert('hi');
+const getAllItems = () => {
+  const items = [
+    { 'id': 'weapon', 'url': 'weapons'},
+    { 'id': 'map', 'url': 'maps' },
+    { 'id': 'headwear', 'url': 'headwear' },
+    { 'id': 'armor', 'url': 'armor'},
+    { 'id': 'rig', 'url': 'rigs'},
+    { 'id': 'backpack', 'url': 'backpacks'}
+  ]
+
+  items.forEach(item => {
+    getItems(item);
+  });
+};
+
+const getItems = (item) => {
+  const url = item.url;
+  fetch(url)
+  .then(data => updateElement(item, data))
+};
+
+const updateElement = (item, data) => {
+  data.text()
+  .then(res => {
+    const itemElement = document.getElementById(item.id).getElementsByClassName('item-name')[0];
+    itemElement.textContent=chooseRandom(JSON.parse(res))
+  })
+};
+
+const chooseRandom = (items) => {
+  return item = items[Math.floor(Math.random() * items.length)];
+  
+};
+
