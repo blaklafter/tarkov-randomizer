cdb5d4df93d1e6989796f3c2a22abea860d91fc1
blaklafter <blaklafter@gmail.com>
2021-02-21 21:40:23 -0500

    Prettier and knobs!

--- public/scripts.js
+++ public/scripts.js
@@ -2,15 +2,22 @@ const items = [
   { 'id': 'weapon', 'url': 'weapons'},
   { 'id': 'map', 'url': 'maps' },
   { 'id': 'headwear', 'url': 'headwear' },
-  { 'id': 'armor', 'url': 'armor'},
+  { 'id': 'backpack', 'url': 'backpacks'},
   { 'id': 'rig', 'url': 'rigs'},
-  { 'id': 'backpack', 'url': 'backpacks'}
-];
+  { 'id': 'armor', 'url': 'armor'}];
+
+const knobs = {};
 
 const getAllItems = () => {
-  items.forEach(item => {
+  itemList = [...items]
+
+  while(itemList.length > 0) {
+    item = itemList.pop();
+    if(item.id !== 'map') {
+      zeroKnob(item.id);
+    }
     getItems(item);
-  });
+  }
 };
 
 const getItems = (item) => {
@@ -18,7 +25,24 @@ const getItems = (item) => {
     item = items.find(i => i.id === item);
     if(!item) {
       console.error(`Invalid data supplied: ${item}`);
-      return
+      return;
+    }
+  }
+
+  const itemElement = (specifiedItem) => document.getElementById(specifiedItem || item.id).querySelectorAll('div.item-name')[0];
+  itemElement().classList.add('add-text');
+
+  setTimeout(() => {itemElement().classList.remove('add-text');}, 1000);
+
+  // map cannot be None
+  if( item.id !== 'map') {
+    selectedChanceForNone = knobs[item.id]
+
+    chanceForNone = selectedChanceForNone || 0;
+
+    if(Math.random() * 100 <= chanceForNone) {
+      updateElement(item, "None");
+      return;
     }
   }
   
@@ -28,14 +52,82 @@ const getItems = (item) => {
 };
 
 const updateElement = (item, data) => {
+  const itemElement = (specifiedItem) => document.getElementById(specifiedItem || item.id).getElementsByClassName('item-name')[0];
+  
+  if(data === 'None') {
+    itemElement().textContent = data;
+    return;
+  }
+
   data.text()
   .then(res => {
-    const itemElement = document.getElementById(item.id).getElementsByClassName('item-name')[0];
-    itemElement.textContent=chooseRandom(JSON.parse(res));
+    selectedItem = chooseRandom(JSON.parse(res));
+
+    // check for armored rig, if it is, make armor "None"
+    if(item.id === 'rig'){
+      if(selectedItem['type'] == 'Armored'){
+        //itemElement('armor').textContent = 'None';
+        delayedUpdate(itemElement('armor'), 'None', 250)
+      }
+      //itemElement().textContent = selectedItem['name'];
+      delayedUpdate(itemElement(), selectedItem['name'], 250)
+      return;
+    }
+
+    //itemElement().textContent = selectedItem;
+    delayedUpdate(itemElement(), selectedItem, 250)
   })
 };
 
-const chooseRandom = (items) => {
-  return item = items[Math.floor(Math.random() * items.length)];
+const delayedUpdate = (element, elementText, msDelay) => {
+    setTimeout(() => {element.textContent = elementText}, msDelay);
+
 };
+
+const chooseRandom = (items) => {
+  
+  const item = items[Math.floor(Math.random() * items.length)];
+  return item
+};
+
+function zeroKnob(elementId) {
+  const defaultKnobValue = 10;
+  // Create knob element, 70 x 70 px in size.
+  const knob = pureknob.createKnob(70, 70);
+  knobs[elementId] = defaultKnobValue;
+
+  // Set properties.
+  knob.setProperty('angleStart', -0.75 * Math.PI);
+  knob.setProperty('angleEnd', 0.75 * Math.PI);
+  knob.setProperty('colorFG', '#88ff88');
+  knob.setProperty('trackWidth', 0.4);
+  knob.setProperty('valMin', 0);
+  knob.setProperty('valMax', 100);
+
+  // Set initial value.
+  knob.setValue(defaultKnobValue);
+
+  /*
+   * Event listener.
+   *
+   * Parameter 'knob' is the knob object which was
+   * actuated. Allows you to associate data with
+   * it to discern which of your knobs was actuated.
+   *
+   * Parameter 'value' is the value which was set
+   * by the user.
+   */
+  const listener = function(knob, value) {
+    knobs[elementId] = value;
+  };
+
+  knob.addListener(listener);
+
+  // Create element node.
+  const node = knob.node();
+
+  // Add it to the DOM.
+  const elem = document.getElementById(elementId).querySelector('div.none-selector');
+  elem.appendChild(node);
+}
 
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 require('dotenv').config();
-const url = process.env.MONGO_CONN_STR
-const dbName = process.env.DATABASE
+const url = process.env.MONGO_CONN_STR;
+const dbName = process.env.DATABASE;
 
 // This is a global variable we'll use for handing the MongoDB client
 var mongodb;
@@ -20,44 +20,46 @@ const findDocuments = async (c, col, filter) => {
   const collection = c.collection(col);
   // Find some documents
   const documents = await collection.find(filter).toArray();
-  return documents
-}
+  return documents;
+};
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use('/', express.static('public'))
+app.use('/', express.static('public'));
 
 app.get('/maps', async (req, res) => {
-  docs = await findDocuments(mongodb, 'maps', {'Release State': 'Released'})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'maps', {'Release State': 'Released', 'Name': {'$ne': 'Hideout'}});
+  res.send(docs.map( d => d['Name']));
+});
 
 app.get('/weapons', async (req, res) => {
-  docs = await findDocuments(mongodb, 'weapons', {})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'weapons', {});
+  res.send(docs.map( d => d['Name']));
+});
 
 app.get('/armor', async (req, res) => {
-  docs = await findDocuments(mongodb, 'armor', {})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'armor', {});
+  res.send(docs.map( d => d['Name']));
+});
 
 app.get('/backpacks', async (req, res) => {
-  docs = await findDocuments(mongodb, 'backpacks', {})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'backpacks', {});
+  res.send(docs.map( d => d['Name']));
+});
 
 app.get('/rigs', async (req, res) => {
-  docs = await findDocuments(mongodb, 'rigs', {})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'rigs', {});
+  res.send(docs.map( d => {
+    return {'name': d['Name'], 'type': d['parsed_type']};
+  }));
+});
 
 app.get('/headwear', async (req, res) => {
-  docs = await findDocuments(mongodb, 'headwear', {})
-  res.send(docs.map( d => d['Name']))
-})
+  docs = await findDocuments(mongodb, 'headwear', {});
+  res.send(docs.map( d => d['Name']));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
